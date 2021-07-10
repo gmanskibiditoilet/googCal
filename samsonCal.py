dd754c7e9cf39f49a26b4b30c750207e396d5e7f
Daniel Swanson <dantheman9991@ymail.com>
2021-07-09 21:18:26 -0600

    Fix email Subject read
          modified:   samsonCal.py

--- samsonCal.py
+++ samsonCal.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
 
-import os, re, requests, sys, subprocess
+import email
+import os
+import re
+import requests
+import subprocess
+import sys
 from datetime import datetime, timedelta
+from email import policy
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-from email.header import make_header, decode_header
 
 debugging = False
+GOOGLE_CALENDAR_MAIL_PATH = "/etc/googleCalendar/mail/"
 
 os.chdir('/etc/googleCalendar/')
 
 headers = {
     'Content-type': 'application/json',
 }
-f=open("/etc/googleCalendar/slack.txt", "r")
+f = open("/etc/googleCalendar/slack.txt", "r")
 if f.mode == 'r':
-    slackapikey=f.read().strip()
+    slackapikey = f.read().strip()
     f.close()
     if debugging:
         print("Slack API Key is "+slackapikey)
 f.close()
 
-
-#check email
+# check email
 try:
     subprocess.check_output(['/usr/bin/fetchmail'])
 except subprocess.CalledProcessError as e:
@@ -35,9 +40,10 @@ except subprocess.CalledProcessError as e:
         print("Something else went wrong: "+str(e.output))
         quit()
 
-def openFile ( filePath ):
+
+def openFile(filePath):
     try:
-        f=open('/etc/googleCalendar/mail/'+filePath, 'r')
+        f = open(GOOGLE_CALENDAR_MAIL_PATH + filePath, 'r')
         lines = f.readlines()
     except:
         if debugging:
@@ -47,15 +53,16 @@ def openFile ( filePath ):
         f.close()
     return lines
 
-#define function for handling new meetings
-def newMeeing ( filePath, groupName ):
+
+# define function for handling new meetings
+def newMeeting(filePath, groupName):
     lines = openFile (filePath)
     for line in lines:
-        #Match Add to list
+        # Match Add to list
         if "Would you like to be added to the distribution" in line:
-            #get everything beginning with https:// and ending with '>
+            # get everything beginning with https:// and ending with '>
             matches = re.findall("https:\/\/.*'>", line)
-            link = str(matches[0])[:-2] #Convert to string and strip the last 2 extraneous characters
+            link = str(matches[0])[:-2] # Convert to string and strip the last 2 extraneous characters
     headers = {
         'Content-type': 'application/json',
     }
@@ -64,7 +71,8 @@ def newMeeing ( filePath, groupName ):
     print(data)
     response = requests.post(slackapikey, headers=headers, data=data)
 
-#Instantiate Google Calendar API
+
+# Instantiate Google Calendar API
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 store = file.Storage('token.json')
 creds = store.get()
@@ -73,81 +81,94 @@ if not creds or creds.invalid:
     creds = tools.run_flow(flow, store)
 service = build('calendar', 'v3', http=creds.authorize(Http()))
 
-
-#Load all the email files into memory
+# Load all the email files into memory
 messages = os.listdir("/etc/googleCalendar/mail")
+
 for message in messages:
-    lines = openFile ( message )
-    #Loop through the lines looking for the info.
-    Proceed = True; groupName = False; dateString = False; zoomLink = False; ModificatonEmail = False
-    for line in lines:
-        if "Subject:" in line:
-            #This is the subject line.
-            if (debugging):
-                print(line)
-            #Check if UTF-8
-            if "ascii" in line or "UTF-8" in line:
-                line = str(make_header(decode_header(line)))
-            if (debugging):
-                print("Decoded from UTF-8 or ASCII")
-                print(line)
-            #Need to check if it is an invitation to next meeting
-            if "Invitation to Next Sa" in line:
-                groupName = line.split(': ')[1]
-            #Check if this is a modification of an existing meeting
-            elif "Samson Meeting Changed" in line:
-                groupName = line.split(': ')[1]
-                ModificatonEmail = True
-                if (debugging):
-                    print("This is a Modification Email")
-            #Need to check if it is an invitation to the first meeting of a new group
-            elif "Your Invitation to Join the Samson Society Group" in line:
-                groupName = line.split(': ')[1]
-                newMeeing( message, groupName)
-                Proceed = False
+    Proceed = True
+    groupName = dateString = zoomLink = ModificationEmail = False
+    try:
+        emailMessage = open(GOOGLE_CALENDAR_MAIL_PATH + message, 'r')
+        # Grab message from the file
+        # We want to use the policy.default which decodes encoded words
+        msg = email.message_from_file(emailMessage, policy=policy.default)
+    except:
+        if debugging:
+            print("Something went wrong opening the file")
+        quit()
+    finally:
+        emailMessage.close()
+    subject = msg['Subject']
+    if debugging:
+        print(subject)
+    # Need to check if it is an invitation to next meeting
+    if "Invitation to Next Samson Society Meeting" in subject:
+        groupName = subject.split(': ')[1]
+    # Check if this is a modification of an existing meeting
+    elif "Samson Meeting Changed" in subject:
+        groupName = subject.split(': ')[1]
+        ModificationEmail = True
+        if debugging:
+            print("This is a Modification Email")
+    # Need to check if it is an invitation to the first meeting of a new group
+    elif "Your Invitation to Join the Samson Society Group" in subject:
+        groupName = subject.split(': ')[1]
+        newMeeting( message, groupName)
+        Proceed = False
+
+    body = ""
+
+    if msg.is_multipart():
+        # Walk through the parts of the message till we get to the body
+        for part in msg.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get('Content-Disposition'))
+            # grab text/html body
+            if ctype == 'text/html' and 'attachment' not in cdispo:
+                body = part.get_payload()
                 break
-        if "ur next meeting will be" in line: #This is a next Meeting Email
-            #This is the paragraph line
-            try:
-                dateString = str(re.findall("((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{2}, \d{4} at \d{2}:\d{2} (AM|PM) \(.*\))",line)[0][0])
-            except:
-                if debugging:
-                    print("No dateString in this email")
-                dateString = False
-                break
-            try:
-                zoomLink = str(re.findall("https:\/\/.*'>", line)[0])[:-2]
-                if not("zoom" in zoomLink):
-                    #link is not a zoom link
-                    zoomLink = False
-            except:
-                if debugging:
-                    print("No zoom link found in this email")
+    if "ur next meeting will be" in body:  # This is a next Meeting Email
+        # Look for the date String
+        try:
+            dateString = str(re.findall(
+                "((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{2}, \d{4} at \d{2}:\d{2} (AM|PM) \(.*\))", body)[
+                                 0][0])
+        except:
+            if debugging:
+                print("No dateString in this email")
+            dateString = False
+        try:
+            zoomLink = str(re.findall("https:\/\/.*'>", body)[0])[:-2]
+            if not ("zoom" in zoomLink):
+                # link is not a zoom link
                 zoomLink = False
-                break
-    #Validate
-    if not(Proceed and dateString and groupName and (ModificatonEmail or zoomLink)):
-        #This email does not contain all necessary info.
-        #Stop processing this email and move on to the next in the loop
+        except:
+            if debugging:
+                print("No zoom link found in this email")
+            zoomLink = False
+    # Validate
+    if not(Proceed and dateString and groupName and (ModificationEmail or zoomLink)):
+        # This email does not contain all necessary info.
+        # Stop processing this email and move on to the next in the loop
         if debugging:
             print ("Invalid Email, moving on")
             print ("Proceed: "+str(Proceed))
             print ("groupName: "+str(groupName))
             print ("dateString: "+str(dateString))
             print ("zoomLink: "+str(zoomLink))
-        os.remove("/etc/googleCalendar/mail/"+message)
+        os.remove(GOOGLE_CALENDAR_MAIL_PATH + message)
         continue
-        
-    #Regular New Meeting Email:
-    
-    #Break the dateString into parts
-    #Strip the timezone:
+
+    # Regular New Meeting Email:
+
+    # Break the dateString into parts
+    # Strip the timezone:
     dateTimeStr = (dateString.split("(")[0][:-1]).replace(',', '').replace('at', '') #yields Mar 08 2019  12:00 PM
     startDate = datetime.strptime(dateTimeStr, '%b %d %Y %I:%M %p')
     endDate = startDate + timedelta(hours=1)
-    #Determine the timezone
+    # Determine the timezone
     timezoneStr = dateString.split("(")[1]
-    if timezoneStr[-1] == ' ': #Remove trailing space if present
+    if timezoneStr[-1] == ' ':  # Remove trailing space if present
         timezoneStr = timezoneStr[:-1]
         if debugging:
             print("Removed trailing space from time zone string")
@@ -157,24 +178,24 @@ for message in messages:
         if debugging:
             print("Removed trailing ) from time zone string")
             print(timezoneStr)
-    #Convert email format timezone to Google format timezoneStr
-    #Website to get formats: http://www.timezoneconverter.com/cgi-bin/zonehelp.tzc
+    # Convert email format timezone to Google format timezoneStr
+    # Website to get formats: http://www.timezoneconverter.com/cgi-bin/zonehelp.tzc
     TimezoneDictionary = {
         'Eastern Time': 'America/New_York',
         'Central Time': 'America/Chicago',
         'Alaska': 'America/Anchorage',
         'Amsterdam, Berlin, Bern, Rome, Stockho': 'Europe/Rome',
-#        'Greenwich Mean Time : London': 'Etc/GMT',
+        # 'Greenwich Mean Time : London': 'Etc/GMT',
         'Greenwich Mean Time : London': 'Europe/London',
         'Pacific Time': 'America/Los_Angeles',
         'Hawaii': 'Pacific/Honolulu',
         'Arizona': 'America/Phoenix',
         'Beijing, Chongqing, Hong Kong, Urumqi': 'Asia/Shanghai',
-        'Seoul':'Asia/Seoul',
-        'Mountain Time':'America/Denver',
-        'Nairobi':'Africa/Nairobi',
-        'Greenwich Mean Time : Dublin':"Europe/Dublin",
-        'Minsk':'Europe/Minsk'
+        'Seoul': 'Asia/Seoul',
+        'Mountain Time': 'America/Denver',
+        'Nairobi': 'Africa/Nairobi',
+        'Greenwich Mean Time : Dublin': "Europe/Dublin",
+        'Minsk': 'Europe/Minsk'
     }
     timezone = TimezoneDictionary.get(timezoneStr, 'Unknown Timezone')
     if timezone == 'Unknown Timezone':
@@ -186,11 +207,11 @@ for message in messages:
         response = requests.post(slackapikey, headers=headers, data=data)
         continue
 
-    #convert datetime objects into strings
+    # Convert datetime objects into strings
     startDateStr = startDate.strftime('%Y-%m-%dT%H:%M:00')
     endDateStr = endDate.strftime('%Y-%m-%dT%H:%M:00')
-    if not(ModificatonEmail):
-        #Not a modification email. Make a new event
+    if not ModificationEmail:
+        # Not a modification email. Make a new event
         if debugging:
             print("Group Name:\t"+groupName+"\nZoom Link:\t"+zoomLink+"\nStart Date:\t"+startDate.strftime("%b %d, %Y  %I:%M %p")+"\nEnd Date:\t"+endDate.strftime("%b %d, %Y  %I:%M %p")+"\nTimezone:\t"+timezone)
         description = zoomLink+"\nCall In Numbers:\n+1-408-638-0968\n+1-646-558-8656"
@@ -213,11 +234,11 @@ for message in messages:
                 ],
             },
         }
-        os.remove("/etc/googleCalendar/mail/"+message)
+        os.remove(GOOGLE_CALENDAR_MAIL_PATH + message)
         try:
             event = service.events().insert(calendarId='primary', body=event).execute()
             if debugging:
-                #Verbose notifications of creation of each event
+                # Verbose notifications of creation of each event
                 headers = {
                     'Content-type': 'application/json',
                 }
@@ -229,41 +250,41 @@ for message in messages:
             if debugging:
                 print("Error posting the event to Google Calendar")
             e = sys.exc_info()[1]
-            input=" - <!channel> EVENT ERROR: "
-            error=str(e)
-            error=re.sub("<|>", "", error)
-            error= error.split("returned",1) #[1]
-            error= error.split("\"")[1]
-            output=groupName+input+error
+            input = " - <!channel> EVENT ERROR: "
+            error = str(e)
+            error = re.sub("<|>", "", error)
+            error = error.split("returned", 1) # [1]
+            error = error.split("\"")[1]
+            output = groupName+input+error
             data = '{"text":"%s"}' % (output)
             print(data)
             response = requests.post(slackapikey, headers=headers, data=data)
-            #response = requests.post('https://hooks.slack.com/services/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', headers=headers, data=data)
+            # response = requests.post('https://hooks.slack.com/services/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', headers=headers, data=data)
     else:
-        #is a modification Email
-        #Get the event ID of the matching event
+        # is a modification Email
+        # Get the event ID of the matching event
         now = datetime.utcnow().isoformat() + 'Z'
         page_token = None
         while True:
             events = service.events().list(calendarId='primary', timeMin=now, pageToken=page_token).execute()
             for event in events['items']:
-                if (debugging):
+                if debugging:
                     print (event['summary'])
                 if (event['summary']).strip() == groupName.strip():
-                    #This is the meeting we are looking for
-                    myEventID=event['id']
-                    if (debugging):
+                    # This is the meeting we are looking for
+                    myEventID = event['id']
+                    if debugging:
                         print(myEventID)
-                        print (event['start'])
+                        print(event['start'])
                     event['start'] = {'dateTime': startDateStr, 'timeZone': timezone}
                     event['end'] = {'dateTime': endDateStr, 'timeZone': timezone}
                     updatedEvent = service.events().update(calendarId='primary', eventId=myEventID, body=event).execute()
-                    os.remove("/etc/googleCalendar/mail/"+message)
+                    os.remove(GOOGLE_CALENDAR_MAIL_PATH + message)
                     break
             page_token = events.get('nextPageToken')
             if not page_token:
                 if debugging:
-                    #Verbose notifications
+                    # Verbose notifications
                     headers = {
                         'Content-type': 'application/json',
                     }
